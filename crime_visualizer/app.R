d7a38276ea4bab80e264d41099903a2389dc25c3
Gabriel Bogo <gabrielmedeirosbogo@gmail.com>
2019-01-14 15:22:54 -0800

    checkpoint before merging cleaned dataset

--- crime_visualizer/app.R
+++ crime_visualizer/app.R
@@ -1,7 +1,8 @@
 library(shiny)
 library(tidyverse)
+library(here)
 
-# data <- read_csv("..")
+clean_data <- read_csv(here("data", "ucr_crime_1975_2015.csv"))
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -9,33 +10,59 @@ ui <- fluidPage(
     titlePanel("Marshall Violent Crime Visualizer", 
                windowTitle = "Crime App"),
     sidebarLayout(
-        sidebarPanel(sliderInput("Date range", "Select your desired date range.",
-                                 min = 0, max = 100, value = c(15, 30), pre="$")),
-        mainPanel(plotOutput("price_hist"),
-                  tableOutput("price_table"))
+        sidebarPanel(
+            selectInput("state", h4("State"), 
+                        choices = list("Choice 1" = 1, "Choice 2" = 2,
+                                       "Choice 3" = 3), selected = 1),
+            selectInput("city", h4("City"), 
+                        choices = list("Choice 1" = 1, "Choice 2" = 2,
+                                       "Choice 3" = 3), selected = 1),
+            dateRangeInput('dateRange',
+                            label = h4('Date range'),
+                            start = Sys.Date() - 2, end = Sys.Date() + 2)
+
+        ),
+        mainPanel(#plotOutput("price_hist"),
+                  #tableOutput("price_table")
+        ),
     )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
     
-    # observe(print(input$priceInput))
+    #For debugging purposes
+    observe(print(input$state))
+    observe(print(input$city))
+    observe(print(input$dateRange))
     # 
-    # bcl_filtered <- reactive(bcl %>% 
-    #                              filter(Price > input$priceInput[1],
-    #                                     Price < input$priceInput[2])
-    # )
+    reactive_data <- reactive(clean_data %>% 
+                                  filter(year >= input$dateRange[1],
+                                         year <= input$dateRange[2],
+                                         state == input$state,
+                                         city == input$city) %>% 
+                                  group_by(state, city, year) %>% 
+                                  summarize(total_homs = sum(homs_sum),
+                                            total_rape = sum(rape_sum),
+                                            total_rob = sum(rob_sum),
+                                            total_agg_ass = sum(agg_ass_sum),
+                                            total = sum)
+                              )
     # 
-    # output$price_hist <- renderPlot(
-    #     bcl_filtered() %>% 
-    #         ggplot(aes(Price)) +
-    #         geom_histogram()
-    # )
+    output$robbery <- renderPlot(
+        reactive_data() %>%
+            ggplot(aes(Price)) +
+            geom_histogram()
+    )
     # 
     # output$price_table <- renderTable(
     #     bcl_filtered()
     # )
-    # 
+    output$dateRangeText  <- renderText({
+        paste("input$dateRange is", 
+              paste(as.character(input$dateRange), collapse = " to ")
+        )
+    })
 }
 
 # Run the application 
